f4d0cc111aac61e7abf3d17d53b605606ecd43a4
dervism <mail@dervis.no>
2017-11-18 12:07:35 +0100

    Rename modules

--- number_provider_language/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module number_language {
-    requires number_api;
-
-    provides no.dervis.numbertotext.api.spi.NumberResourcesProvider
-            with no.dervis.numbertotext.language.impl.NumberResourcesProviderImpl;
-}
--- number_user_generator/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module number_generator {
-    requires number_api;
-
-    exports no.dervis.numbertotext.generator;
-
-    uses no.dervis.numbertotext.api.spi.NumberResourcesProvider;
-}
