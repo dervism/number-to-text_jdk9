74e61041a2e0ba4571d68a4dd06a0d5fef81c68a
dervism <mail@dervis.no>
2017-11-19 14:37:25 +0100

    Adding Maven exec plugin.

--- number_main/src/main/java/no/dervis/numbertotext/generator/Main.java
+++ number_main/src/main/java/no/dervis/numbertotext/generator/Main.java
@@ -1,12 +1,14 @@
 package no.dervis.numbertotext.generator;
 
+import no.dervis.numbertotext.api.generator.Generator;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Generator generator = new Generator();
+        Generator base10Generator = new Base10Generator();
 
-        System.out.println(generator.convert(123));
+        System.out.println(base10Generator.convert(Integer.parseInt(args[0])));
 
     }
 
